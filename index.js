2943622a32830b72abacbb205a59018c638ae5da
Mortis66666 <mortisloverbs@gmail.com>
2023-03-10 15:53:15 +0800

    .

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const { app } = require('deta');
+const express = require("express");
+const app = express();
 const { MongoClient } = require("mongodb");
 const uri = process.env.uri;
 const client = new MongoClient(uri);
+const port = parseInt(process.env.PORT);
 
-app.lib.cron(async event => {
-    const deletes = [];
+app.post("/__space/v0/actions", async (req, res) => {
+    const event = req.body.event;
 
-    try {
-        const rooms = client.db("db").collection("rooms");
+    if (event.id === "cleanup") {
+        const deletes = [];
 
+        try {
+            const rooms = client.db("db").collection("rooms");
 
-        rooms.find().forEach(room => {
-            let ms = Date.now() - room.lastUpdate;
+            rooms.find().forEach(room => {
+                let ms = Date.now() - room.lastUpdate;
 
-            if (ms > 20 * 60 * 1000) {
-                deletes.push(room._id);
-            }
-        });
+                if (ms > 20 * 60 * 1000) {
+                    deletes.push(room._id);
+                }
+            });
 
-        rooms.deleteMany({
-            _id: {
-                $in: deletes
-            }
-        });
-
-    } finally {
-        console.log(`Deleted ${deletes.length} inactive rooms`);
+            rooms.deleteMany({
+                _id: {
+                    $in: deletes
+                }
+            });
+        } finally {
+            console.log(`Deleted ${deletes.length} inactive rooms`);
+        }
     }
 });
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Background Worker listening on port ${port}`);
+});
